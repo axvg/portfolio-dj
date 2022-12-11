f2337520c27b7f079f20e01d4083f29cd6e2e2ad
axvg <91773368+axvg@users.noreply.github.com>
2022-12-11 16:39:49 -0500

    Add static dir

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# conf
+STATICFILES_DIRS = [BASE_DIR / "static"]
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
