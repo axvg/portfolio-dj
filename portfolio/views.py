3ea873ccffcd08e4c2248833e6d55908b008fa66
axvg <91773368+axvg@users.noreply.github.com>
2022-12-11 19:29:41 -0500

    Add profile model

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,10 @@ from portfolio.models import Profile
 
 class PortfolioView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        profile = Profile.objects.get(id=1)
+
+        context = super().get_context_data(**kwargs)
+        context['profile'] = profile
+        return context
